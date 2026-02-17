9a065ecc4f293c1e65194162bbcea9fcce922734
germanlion67 <143081644+germanlion67@users.noreply.github.com>
2026-02-17 14:51:59 +0100

    Fix formatting in import statements

--- script/immich-ultra-sync.py
+++ script/immich-ultra-sync.py
@@ -34,7 +34,7 @@ from api import (
 )
 from exif import (
     ExifToolHelper, check_exiftool,
-    build_exif_args, get_current_exif_values, extract_desired_values, normalize_exif_value
+    build_exif_args, get_current_exif_values, extract_desired_values, normalize_exif_value,
     execute_with_sidecar_and_msphoto
 )
 
